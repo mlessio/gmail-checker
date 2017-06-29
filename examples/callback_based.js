d1d930ea5636f6a2f3b5cc0e368ab41f7a9d5dfd
Martino Lessio <martino.lessio@mindedsecurity.com>
2017-06-29 19:12:12 +0200

    examples

--- examples/callback_based.js
+++ examples/callback_based.js
@@ -1,8 +1,19 @@
 
 var GmailChecker = require("../index");
 
-//
+//checks for an existing address
 GmailChecker.check('testaddress', function(err, data){
+  if(err){
+    console.error(err);
+    return;
+  }
+  console.log("Username exists:", data.exists);
+  if(data.exists)
+    console.log("Error message", data.message);
+});
+
+//checks for an, hopefully, unexisting address
+GmailChecker.check('testaddress12as12e4dfdf8ugsd', function(err, data){
   if(err){
     console.error(err);
     return;
--- examples/promise_based.js
+++ examples/promise_based.js
@@ -1,7 +1,16 @@
 var GmailChecker = require("../index");
 
-//
+//checks for an existing address
 GmailChecker.check('testaddress').then(function(data){
+  console.log("Username exists:", data.exists);
+  if(data.exists)
+    console.log("Error message", data.message);
+}).catch(function(err){
+  console.error(err);
+});
+
+//checks for an, hopefully, unexisting address
+GmailChecker.check('testaddress12as12e4dfdf8ugsd').then(function(data){
   console.log("Username exists:", data.exists);
   if(data.exists)
     console.log("Error message", data.message);
